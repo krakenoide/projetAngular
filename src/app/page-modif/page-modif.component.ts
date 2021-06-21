e40ffd40563d23e3e6f72097044157115901781d
krakenoide <guillaume.bortoli@gmail.com>
2021-06-21 16:06:03 +0200

    page modification

--- src/app/page-modif/page-modif.component.ts
+++ src/app/page-modif/page-modif.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { ormBuilder, FormGroup, NgForm, Validators, FormBuilder } from '@angular/forms';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-page-modif',
   templateUrl: './page-modif.component.html',
   styleUrls: ['./page-modif.component.css']
 })
-export class PageModifComponent implements OnInit {
 
-  constructor() { }
+export class PageModifComponent implements OnInit {
+  connectedUser: string ="";
+
+  myForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.myForm=this.formBuilder.group({
+      nusername:["",[Validators.required, Validators.maxLength(50)]],
+      oldpassword:["",[Validators.required]],
+      npassword:["",[Validators.required]],
+      npasswordbis:["",[Validators.required]]
+         
+    })
+  }
+
+  onSubmit(): void {
+    
   }
 
 }

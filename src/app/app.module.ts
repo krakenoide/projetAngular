5e5f4ea379f3745a11d2ad86a19d605eb56aae6c
krakenoide <guillaume.bortoli@gmail.com>
2021-06-22 14:46:29 +0200

    merge with brice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { serviceUser } from './Services/serviceUser'
 import { serviceTopic } from './Services/serviceTopic'
 import { serviceMessage } from './Services/serviceMessage'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageModifComponent } from './components/page-modif/page-modif.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LoginPageComponent } from './components/login-page/login-page.component';
 import { HttpClientModule } from'@angular/common/http';
-import { HomePageComponent } from './components/home-page/homepage.component';
 import { CreationPageComponent } from './components/creation-page/creationpage.component';
+import { HomePageComponent } from './components/home-page/homepage.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { PageModifComponent } from './components/page-modif/page-modif.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageModifComponent,
-    LoginPageComponent,
-    HomePageComponent,
     CreationPageComponent,
-    HeaderComponent
-
+    HomePageComponent,
+    HeaderComponent,
+    LoginPageComponent,
+    PageModifComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule

159091f1485f65d9ec89c412e4760c2062608934
Brice <brice.lecomte0@gmail.com>
2021-06-22 10:54:11 +0200

    nouveau commit de test angular

---
+++ src/app/components/creation-page/creationpage.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, NgForm, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-creation-page',
+  templateUrl: './creationpage.component.html',
+  styleUrls: ['../../style.css']
+})
+
+export class CreationPageComponent implements OnInit {
+  myForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+  }
+
+  ngOnInit(): void {
+    this.myForm=this.formBuilder.group({
+      nusername:["",[Validators.required,Validators.maxLength(5)]]
+    });
+    
+  }
+
+  onSubmit(): void {
+    console.log(this.myForm.controls.username);
+  }
+
+  getErrors():string|void {
+    if (this.myForm.controls.username.hasError("required")){
+        return "ce champ est requis";
+    }
+    if (this.myForm.controls.username.hasError("maxLength")) {
+        return "vous avez depassez le nombre de caractères maximum";
+    }
+  }
+}
--- src/app/components/page-modif/page-modif.component.ts
+++ src/app/components/page-modif/page-modif.component.ts
@@ -24,7 +24,7 @@ export class PageModifComponent implements OnInit {
       npassword:["",[Validators.required]],
       npasswordbis:["",[Validators.required]]
          
-    })
+    });
     
   }
 

cdd37af97aadc2201b66bca65f7a345598447259
AntoineWeberDB <weber.antoine.db@gmail.com>
2021-06-22 11:00:41 +0200

    11h00

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder,Validators } from '@angular/forms';
-
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
+
 })
 
-export class LoginPageComponent{
+export class LoginPageComponent implements OnInit{
   loginForm!: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { 

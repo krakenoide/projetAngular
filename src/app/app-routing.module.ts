b2915ad1a1f227b7920b0bee8a7e0ca258a2c3bd
AntoineWeberDB <weber.antoine.db@gmail.com>
2021-06-22 10:47:59 +0200

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login',component: LoginPageComponent}
+];
 
 @NgModule({
+  declarations:[AppComponent],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  providers:[],
+  exports: [RouterModule],
+  bootstrap:[AppComponent]
 })
 export class AppRoutingModule { }

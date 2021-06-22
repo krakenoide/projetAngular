e0186abae2d7062e2c06fc1049eff7e3211a71d2
AntoineWeberDB <weber.antoine.db@gmail.com>
2021-06-22 10:42:46 +0200

    day 536, still not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-=======
 import {serviceUser} from './Services/serviceUser'
 import {serviceTopic} from './Services/serviceTopic'
 import {serviceMessage} from './Services/serviceMessage'
@@ -13,26 +7,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageModifComponent } from './page-modif/page-modif.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
->>>>>>> 9f3bcb2b2d33e8b5ba7d28a2da3c8c7cbc16ff57
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    LoginPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    AppRoutingModule
-=======
     PageModifComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
->>>>>>> 9f3bcb2b2d33e8b5ba7d28a2da3c8c7cbc16ff57
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [serviceUser,
               serviceMessage,
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,54 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup,NgForm,Validators} from '@angular/forms'
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
-  myForm!: FormGroup;
-  message = "bonjour";
-  isAuth = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+export class LoginPageComponent{
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+  }
+
 
   ngOnInit(): void {
     console.log("ngOnInit");
-    this.myForm = this.formBuilder.group({
+    this.loginForm = this.formBuilder.group({
       username: ['',[Validators.required,Validators.minLength(3),Validators.maxLength(50)]],
       password: ['',[Validators.required,Validators.minLength(3),Validators.maxLength(50)]]
     });
   } 
 
-  getUsernameErrors(): string|void{
-    if (this.myForm.controls.username.hasError('required')){
+
+  onSubmit(): void {
+    console.log('onSubmit');
+    console.log(this.loginForm.value.username);
+    console.log(this.loginForm.value.password);
+  }
+
+   getUsernameErrors(): string|void{
+    if (this.loginForm.controls.username.hasError('required')){
       return 'Ce champ est requis';
     }
-    if (this.myForm.controls.username.hasError('minLength')){
+    if (this.loginForm.controls.username.hasError('minLength')){
       return "Le nom d'utilisateur doit comporter au minimum 3 caractères";
     }
-    if (this.myForm.controls.username.hasError('maxLength')){
+    if (this.loginForm.controls.username.hasError('maxLength')){
       return "Le nom d'utilisateur doit comporter au maximum 50 caractères";
     }
   }
   
   getPasswordErrors(): string|void{
-    if (this.myForm.controls.password.hasError('required')){
+    if (this.loginForm.controls.password.hasError('required')){
       return 'Ce champ est requis';
     }
-    if (this.myForm.controls.password.hasError('minLength')){
+    if (this.loginForm.controls.password.hasError('minLength')){
       return "Le mot de passe doit comporter au minimum 3 caractères";
     }
-    if (this.myForm.controls.password.hasError('maxLength')){
+    if (this.loginForm.controls.password.hasError('maxLength')){
       return "Le mot de passe doit comporter au maximum 50 caractères";
     }
   }
 
-  onSubmit(): void {
-      console.log('onSubmit');
-      this.isAuth =true;
-      console.log(this.myForm.value);
-  }
 
+  
 }
+
+
+  
+

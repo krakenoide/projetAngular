f8fbc41605f7ca6da74d9dce98aad6035e0f53fc
Amonjean <adrien.monjean@gmail.com>
2021-06-23 12:13:28 +0200

    commit messageservice premier finition sans test

--- src/app/Services/serviceMessage.ts
+++ src/app/Services/serviceMessage.ts
@@ -4,51 +4,68 @@ import { HttpClient } from "@angular/common/http";
 import { User } from "src/app/modeles/User";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Message} from "src/app/modeles/Message";
+import { Topic} from "src/app/modeles/Topic";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Subscription } from 'rxjs';
+import { serviceUser } from '../Services/serviceUser';
 
 
 @Injectable()
 export class serviceMessage {
-    m : Message;
+    activeMessage!: Message;
     messages! : Message[];
     apiMessage = "http://localhost:8080/api/message"
-    urlLogin = "http://localhost:8080/topic"
+    connectedUser!: User;
 
-    constructor(private httpClient:HttpClient,private router:Router){}
+    constructor(private httpClient:HttpClient,private router:Router,private snackBar:MatSnackBar){}
    
 
 
-    getUser(id : number):void {
-        this.user=cuser;
-    }
 
-    emitUser():string{
-        return this.user;
-    }
+    
     getAllmessages() {
-        this.httpClient.get<User[]> (this.apiMessage)
+        this.httpClient.get<Message[]> (this.apiMessage)
                        .subscribe(data =>{this.messages=data},
                         error => {
+                            this.snackBar.open("Echec de la srecupération de tous les messages!");
+
 
                         });
     }
-    getMessage() {
-        this.httpClient.get<User[]> (this.message)
-                       .subscribe(data =>{this.users=data},
+    getMessage(id:number) {
+        this.httpClient.get<Message> (this.apiMessage+`/${id}`)
+                       .subscribe(data =>{this.activeMessage=data},
                         error => {
+                            this.snackBar.open("Echec de la recupération d'un message!");
+
 
                         });
     }
-    createUser(username:string,password:string,passwordbis:string){
-        this.httpClient.post<User> (this.apiUser, {username:username,password:password,passwordbis:passwordbis})
-                       .subscribe(data =>{this.connectedUser=data; this.emitConnectedUser();   this.redirectToHomePage();
+    createMessage(Content:string){
+        this.httpClient.post<Message> (this.apiMessage, {content: Content, user: this.connectedUser, date: Date, "topic": Topic})
+                       .subscribe(data =>{this.activeMessage=data;  
                         
                     },
                        error => {
+                        this.snackBar.open("Echec de la création!");
+
 
                        });
                     
     
                     }
+
+      modifMessage(ncontent:string){
+         this.httpClient.patch<Message> (this.apiMessage+`/${this.activeMessage.id}`, {content:ncontent})
+             .subscribe(data =>{this.activeMessage=data; 
+            
+                                    },
+         error => {
+            this.snackBar.open("Echec de la modification!");
+
+                                          
+                 });
+         }
 
 
 
@@ -56,6 +73,7 @@ export class serviceMessage {
         this.httpClient.delete(this.apiMessage+`/${id}`)
                        .subscribe(data =>{},
                         error => {
+                            this.snackBar.open("Echec de la supression!");
 
                         });
 }

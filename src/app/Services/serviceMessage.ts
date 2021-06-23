d42bc665cb451c9f03850cf76989a4dbf6296533
Amonjean <adrien.monjean@gmail.com>
2021-06-23 11:12:48 +0200

    service message temporaire

--- src/app/Services/serviceMessage.ts
+++ src/app/Services/serviceMessage.ts
@@ -1,10 +1,22 @@
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { User } from "src/app/modeles/User";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Message} from "src/app/modeles/Message";
+
 
 @Injectable()
 export class serviceMessage {
-    user: string="";
+    message :
+    messages! : Message[];
     userSubject = new Subject<string>();
+    apiMessage = "http://localhost:8080/api/message"
+    urlLogin = "http://localhost:8080/topic"
+
+    constructor(private httpClient:HttpClient,private router:Router){}
+   
+
 
     getUser(cuser:string):void {
         this.user=cuser;
@@ -13,4 +25,39 @@ export class serviceMessage {
     emitUser():string{
         return this.user;
     }
+    getAllmessages() {
+        this.httpClient.get<User[]> (this.apiMessage)
+                       .subscribe(data =>{this.messages=data},
+                        error => {
+
+                        });
+    }
+    getMessage() {
+        this.httpClient.get<User[]> (this.message)
+                       .subscribe(data =>{this.users=data},
+                        error => {
+
+                        });
+    }
+    createUser(username:string,password:string,passwordbis:string){
+        this.httpClient.post<User> (this.apiUser, {username:username,password:password,passwordbis:passwordbis})
+                       .subscribe(data =>{this.connectedUser=data; this.emitConnectedUser();   this.redirectToHomePage();
+                        
+                    },
+                       error => {
+
+                       });
+                    
+    
+                    }
+
+
+
+    deleteUser(id:number){
+        this.httpClient.delete(this.apiMessage+`/${id}`)
+                       .subscribe(data =>{},
+                        error => {
+
+                        });
+}
 }

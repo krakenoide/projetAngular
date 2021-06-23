d0601afb9eca250ae41e46f5f5d7ef32602c9b0d
AntoineWeberDB <weber.antoine.db@gmail.com>
2021-06-23 10:12:54 +0200

    why was I in main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { serviceUser } from './Services/serviceUser'
 import { serviceTopic } from './Services/serviceTopic'
@@ -6,14 +6,13 @@ import { serviceMessage } from './Services/serviceMessage'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClientModule } from'@angular/common/http';
 import { CreationPageComponent } from './components/creation-page/creationpage.component';
 import { HomePageComponent } from './components/home-page/homepage.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { PageModifComponent } from './components/page-modif/page-modif.component';
-
 
 
 @NgModule({
@@ -23,7 +22,7 @@ import { PageModifComponent } from './components/page-modif/page-modif.component
     HomePageComponent,
     HeaderComponent,
     LoginPageComponent,
-    PageModifComponent
+    PageModifComponent 
   ],
   imports: [
     BrowserModule,

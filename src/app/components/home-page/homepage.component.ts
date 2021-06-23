ba1fb3268ae3b6ed4570e04acc447c6c56600307
Brice <brice.lecomte0@gmail.com>
2021-06-23 11:02:52 +0200

    avec message que l'on peut aggrandir

--- src/app/components/home-page/homepage.component.ts
+++ src/app/components/home-page/homepage.component.ts
@@ -10,11 +10,43 @@ import { serviceUser } from '../../Services/serviceUser';
 
 export class HomePageComponent implements OnInit {
   connectedUser!: User;
+  myForm!: FormGroup;
 
-  constructor(private services:serviceUser) { 
+  constructor(private formBuilder: FormBuilder,private services:serviceUser) { 
   }
 
-  ngOnInit(): void {     
+  ngOnInit(): void {   
+    this.myForm = this.formBuilder.group({
+      title: ['',[Validators.required,Validators.minLength(5),Validators.maxLength(100)]],
+      message: ['',[Validators.required,Validators.minLength(5),Validators.maxLength(3000)]]
+    });  
+  }
+
+  onSubmit(): void {
+  }
+
+  getTitleErrors(): string|void{
+    if (this.myForm.controls.title.hasError('required')){
+      return 'Ce champ est requis';
+    }
+    if (this.myForm.controls.title.hasError('minLength')){
+      return "Le titre doit comporter au minimum 5 caractères";
+    }
+    if (this.myForm.controls.username.hasError('maxLength')){
+      return "Le titre doit comporter au maximum 100 caractères";
+    }
+  }
+
+  getMessageErrors(): string|void{
+    if (this.myForm.controls.message.hasError('required')){
+      return 'Ce champ est requis';
+    }
+    if (this.myForm.controls.message.hasError('minLength')){
+      return "Le titre doit comporter au minimum 5 caractères";
+    }
+    if (this.myForm.controls.message.hasError('maxLength')){
+      return "Le titre doit comporter au maximum 3000 caractères";
+    }
   }
   
 }

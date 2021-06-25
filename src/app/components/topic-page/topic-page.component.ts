3f222a1641e0fcdd04e7bfc45944a6778e5bf51d
AntoineWeberDB <weber.antoine.db@gmail.com>
2021-06-25 10:04:44 +0200

    more indentation in topicPage

--- src/app/components/topic-page/topic-page.component.ts
+++ src/app/components/topic-page/topic-page.component.ts
@@ -9,50 +9,45 @@ import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
-  selector: 'app-topic-page',
-  templateUrl: './topic-page.component.html',
-
+	selector: 'app-topic-page',
+	templateUrl: './topic-page.component.html',
 })
 export class TopicpageComponent implements OnInit {
-  myform!: FormGroup;
-  myrefreshbutton! : FormGroup;
-  activeTopic !: Topic;
-  topicsubscription !: Subscription;
-  activeid !: number;
+	myform!: FormGroup;
+	myrefreshbutton! : FormGroup;
+	activeTopic !: Topic;
+	topicsubscription !: Subscription;
+	activeid !: number;
 
+	constructor(private formbuilder : FormBuilder, private services:serviceTopic,
+			private route : ActivatedRoute) {
+	}
+	
+	ngOnInit(): void {
+		// creer une propriété topic 
+		// souscrire au topic subject et dans le subscribe  assigner le retour du subscribe a la propriété topic
+		// faire un appel de emitactivetopic
+		
+		const topic_id = this.route.snapshot.params['id'];
 
+		this.services.getTopic(topic_id).subscribe((currentTopic :Topic) => {
+			this.activeTopic= currentTopic;
+		});
 
-  constructor(private formbuilder : FormBuilder, private services:serviceTopic, private route : ActivatedRoute) {
-   
-   }
-  
-  ngOnInit(): void {
-    // creer une propriété topic 
-    // souscrire au topic subject et dans le subscribe  assigner le retour du subscribe a la propriété topic
-    // faire un appel de emitactivetopic
-    
-    const topic_id = this.route.snapshot.params['id'];
+		this.myform=this.formbuilder.group({
+			content:['',[Validators.required,Validators.maxLength(3000),Validators.minLength(5)]]
+		});
+		this.myrefreshbutton=this.formbuilder.group({});
 
-    this.services.getTopic(topic_id).subscribe((currentTopic :Topic) => {
-      this.activeTopic= currentTopic;
-    });
+	}
 
-    this.myform=this.formbuilder.group({
-      content:['',[Validators.required,Validators.maxLength(3000),Validators.minLength(5)]]
-    
+	onSubmit(): void{
+		console.log(this.myform.value);
+	}
 
-    });
-    this.myrefreshbutton=this.formbuilder.group({});
-
-  }
-
-onSubmit(): void{
-  console.log(this.myform.value);
-}
-
-refresh(): void {
-  console.log("j'ai refresh");
-  //getAllmessages()
-}
+	refresh(): void {
+		console.log("j'ai refresh");
+		//getAllmessages()
+	}
 
 }

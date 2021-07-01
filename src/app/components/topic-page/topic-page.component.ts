73f08669c0847d025aa93912be613841a60c7798
krakenoide <guillaume.bortoli@gmail.com>
2021-07-01 09:51:34 +0200

    correction topic page

--- src/app/components/topic-page/topic-page.component.ts
+++ src/app/components/topic-page/topic-page.component.ts
@@ -42,14 +42,9 @@ export class TopicpageComponent implements OnInit {
   }
 
 onSubmit(): void{
-<<<<<<< HEAD
-	this.servicesmessage.createMessage(this.myform.value.content,25062021);  
-
-=======
   let dateTime:number = new Date().getTime();
   this.servicesmessage.createMessage(this.myform.value.content,dateTime);
   this.refresh();
->>>>>>> 3f2498dcd40588e779d47f25bf87627e1636c0d6
 }
 
 refresh(): void {
